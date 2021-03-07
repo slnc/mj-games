a754dcf14630c7cef6118e4f7f23b3ea00069e48
slnc <juan@juanalonso.net>
2021-03-07 14:26:51 +0100

    minor cleanups

--- generate_npc.py
+++ generate_npc.py
@@ -13,25 +13,27 @@ _NUM_NPCs = 10  # number of NPCs to generate
 _P_FEMALE = 0.5  # probability of the NPC being female
 # END CONFIG
 
-# - Profession is the name of the profession.
-# - p_male is the probability of this profession being chosen for a male NPC.
+# The CSV must have 3 columns called:
+# - profession: name of a profession.
+# - p_male: probability of this profession being chosen for a male NPC.
 #     The sum of all p_male must be exactly 1.
-# - p_female is like p_male but for female NPCs.
-#
-# TODO: every time a new profession is added or removed the whole
-# table of frequencies needs to be updated. An alternative could be
-# to make this column represent a factor and choose 1.0 to mean
-# "this profession is as likely as the average profession", and that
-# way new professions can be added / removed without having to modify
-# the whole frequencies table. The disadvantage is that it'sn harder to think in
-# terms of an abstract "average" profession vs thinking in terms of population
-# totals.
+# - p_female: like p_male but for female NPCs.
+
+# TODO: every time a new profession is added or removed the whole table of
+# frequencies needs to be updated. An alternative could be to make this column
+# represent a factor and choose 1.0 to mean "this profession is as likely as the
+# average profession", and that way new professions can be added / removed
+# without having to modify the whole frequencies table. The disadvantage is that
+# it'sn harder to think in terms of an abstract "average" profession vs thinking
+# in terms of population totals.
 _PROFESSIONS_CSV = '''profession,p_male,p_female
 blacksmith,0.7,0.1
 innkeeper,0.2,0.2
 cook,0.1,0.7'''
 
-# Other constants
+_CSV_P_MALE_COLUMN_NAME = 'p_male'
+_CSV_P_FEMALE_COLUMN_NAME = 'p_female'
+
 _MALE = 'male'
 _FEMALE = 'female'
 
@@ -64,15 +66,15 @@ def generate_profession(gender):
 
         last_profession = profession
 
-    # We need this to handle floating point rounding errors.
-    return last_profession
+    return last_profession  # needed to handle float rounding errors
 
 
 def get_probability(profession, gender):
     '''Returns the probability of a profession given the NPC's gender.'''
-    p_column = 'p_male'
+    p_column = _CSV_P_MALE_COLUMN_NAME
     if gender == _FEMALE:
-        p_column = 'p_female'
+        p_column = _CSV_P_FEMALE_COLUMN_NAME
+
     return float(profession[p_column])
 
 
